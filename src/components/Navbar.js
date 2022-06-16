2196d8247de0459047fce813f05e3194182c17ed
indybrick <72779484+indybrick@users.noreply.github.com>
2022-06-15 17:14:33 -0700

    Added Navbar, home page, and links to other pages.

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import Logo from '../assets/brickLogo.png';
+import {Link} from 'react-router-dom';
+import "../styles/Navbar.css";
+import ReorderIcon from '@mui/icons-material/Reorder';
+
+function Navbar() {
+    
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavBar = () => {
+    setOpenLinks(!openLinks);
+  };
+  return (
+    <div className='navbar'>
+        <div className='leftSide' id={openLinks ? "open" : "close"}>
+            <img src={Logo} />
+            <div className='hiddenLinks'>
+              <Link to="/"> Home </Link>
+              <Link to="/projects"> Projects </Link>
+              <Link to="/resume"> Resume </Link>
+              <Link to="/contact"> Contact </Link>
+            </div>
+        </div>
+        <div className='rightSide'>
+              <Link to="/"> Home </Link>
+              <Link to="/projects"> Projects </Link>
+              <Link to="/resume"> Resume </Link>
+              <Link to="/contact"> Contact </Link>
+            <button onClick={toggleNavBar}>
+              <ReorderIcon />
+            </button>
+            
+        </div>
+    </div>
+  );
+}
+
+export default Navbar
